b2437f6cd41b7e05a5edce602be3c9df03050ecf
Mansoori Simran <62114503+simmimansoori@users.noreply.github.com>
2021-06-14 21:09:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const weatherApi = {
+  key: "334b22e1b3520ba1dd2382d1e8fdef8f",
+  baseurl: "https://api.openweathermap.org/data/2.5/weather",
+};
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+
+const searchInputBox = document.getElementById("input-box");
+
+searchInputBox.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    console.log(searchInputBox.value);
+    getWeatherReport(searchInputBox.value);
+  }
+});
+
+function getWeatherReport(city) {
+  fetch(`${weatherApi.baseurl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(showWeatherReort);
+}
+
+function showWeatherReort(weather) {
+  console.log(weather);
+
+  let city = document.getElementById("city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let temprature = document.getElementById("temp");
+  temprature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+  let weatherType = document.getElementById("weather");
+  weatherType.innerText = `${weather.weather[0].main}`;
+
+  let date = document.getElementById("date");
+  let todaydate = new Date();
+  date.innerText = dateManage(todaydate);
+}
+
+function dateManage(dateArg) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let year = dateArg.getFullYear();
+  let month = months[dateArg.getMonth()];
+  let date = dateArg.getDate();
+
+  return `${date} ${month} ${year}`;
+}
+
